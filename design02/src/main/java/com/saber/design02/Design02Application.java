0638a031f0e6dd8932510a62712deb322adecf9a
DESKTOP-78OU2IK\h1854 <185482337@qq.com>
2022-12-06 00:49:49 +0800

    创建项目

---
+++ design02/src/main/java/com/saber/design02/Design02Application.java
@@ -0,0 +1,13 @@
+package com.saber.design02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Design02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Design02Application.class, args);
+    }
+
+}
